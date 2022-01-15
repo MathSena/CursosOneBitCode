250a1896fa769c35845ce39c1cce89490a6493df
MathSena <MathSena07@hotmail.com>
2022-01-14 21:25:46 -0300

    Conhecendo o Switch

---
+++ javascript/switch.js
@@ -0,0 +1,28 @@
+let velocity = 80
+
+switch (velocity * 2) {
+  case 100:
+    console.log('100')
+    break
+  case 160:
+    console.log('200')
+    break
+  default:
+    console.log('Velocidade não encontrada!')
+}
+
+let spaceship = 'Elemental'
+
+switch (spaceship) {
+  case 'Golias':
+    console.log('Nave mais resistente')
+    break
+  case 'Raptor':
+    console.log('Nave mais rápida')
+    break
+  case 'Enterpise':
+    console.log('Nave veloz')
+    break
+  default:
+    console.log('Nave comum')
+}
